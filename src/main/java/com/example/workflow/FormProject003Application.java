d993ccc2db8cd58862c807cac80fef5cb10005e5
LukkasGN <lucasgnordio@gmail.com>
2026-05-13 22:51:14 +0100

    first commit

---
+++ src/main/java/com/example/workflow/FormProject003Application.java
@@ -0,0 +1,13 @@
+package com.example.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormProject003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormProject003Application.class, args);
+    }
+
+}
---
+++ start-flowset-control.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\Users\LukkasGN\IdeaProjects\FlowSetControlGIT\flowset-control-community
+start "Flowset Control" gradlew.bat bootRun
